79c04830fb89062c89426a19d3ea6a853101ca5a
Joey Pinaroc <jmpinaroc123@gmail.com>
2024-03-08 14:34:36 -0700

    added change

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace FinalProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("change");
         }
     }
 }
